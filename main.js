df48d303aacbffba770df33e0a322f65f03c1f09
John <jaricaya@gmail.com>
2024-03-23 18:30:16 -0700

    complete

--- main.js
+++ main.js
@@ -1,70 +1,48 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = '♡';
+const FULL_HEART = '♥';
 const errorModal = document.querySelector("#modal");
+const likeButtons = document.querySelectorAll(".like-glyph");
 
-// Your JavaScript code goes here!
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("DOM CONTENT HAS LOADED")
-  // Add the `.hidden` class to the error modal in the HTML so it does not appear when the page first loads
-  // const errorModal = document.querySelector("#modal")
-  errorModal.classList.add("hidden")
-  console.log(errorModal)
-  //call find likes
+  errorModal.classList.add("hidden");
+
   findLikes();
-})
+});
 
-function hideError(){
-  errorModal.classList.add("hidden")
-}
-function findLikes(){
-  const likeArr = document.querySelectorAll(".like-glyph")
-  likeArr.forEach((singularLike) => {
-    singularLike.addEventListener("click", () => console.log("YOU FOUND ME! Like!") )
-  })
+function hideError() {
+  errorModal.classList.add("hidden");
 }
 
-  function clickListener() {
-      document.addEventListener('click', (event) => {
-
-
-        if (event.target.classList[0] === 'like-glyph') {
-    mimicServerCall()
-    
-    .then(resp => {
-      event.target.classList.add("activated-heart")
-    })
-    .catch(error => {
-      errorModal.classList.remove("hidden")
-      setTimeout(() => {
-        hideError()}, 3000)
-    })
-  }
-      });
-    }
-
-
-
-//------------------------------------------------------------------------------
-// Don't change the code below: this function mocks the server response
-//------------------------------------------------------------------------------
-
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
-      if (isRandomFailure) {
-        reject("Random server error. Try again.");
-      } else {
-        resolve("Pretend remote server notified of action!");
-      }
-    }, 300);
+function findLikes() {
+  likeButtons.forEach((likeButton) => {
+    likeButton.addEventListener("click", () => {
+      mimicServerCall()
+        .then(() => {
+          likeButton.classList.toggle("activated-heart");
+        })
+        .catch((error) => {
+          errorModal.classList.remove("hidden");
+          setTimeout(hideError, 3000);
+        });
+    });
   });
 }
 
-
-
-
+function mimicServerCall() {
+  // Simulating a server call with a Promise
+  return new Promise((resolve, reject) => {
+    // Simulating a random server response
+    setTimeout(() => {
+      const random = Math.random();
+      if (random < 0.5) {
+        resolve();
+      } else {
+        reject("Error: Server Call Failed");
+      }
+    }, 500);
+  });
+}
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
